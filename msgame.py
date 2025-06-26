8ca35b586744399eed92773b01b6fe0f925bdfd8
joe <wu.joe@outlook.com>
2025-06-26 16:59:47 +0800

    #1

--- msgame.py
+++ msgame.py
@@ -1,2 +1,25 @@
-
-mines = []
+import random
+gezi = []
+for i in range(9):
+    gezi.append([])
+    for j in range(9):
+       gezi[i].append(False)
+d=82
+for g in range(10):
+    c=random.randint(0,81)
+    if c==d:
+        c=random.randint(0,81)
+    elif c==d:
+        c=random.randint(0,81)
+    elif c==d:
+        c=random.randint(0,81)
+    elif c==d:
+        c=random.randint(0,81)
+    else:
+        e=c//9
+        if e==9:
+            e=e-1
+        f=c%9
+        gezi[e][f]=True
+        d=e
+print(gezi)
