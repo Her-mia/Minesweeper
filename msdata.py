386dab5a5d6ecc4cbd0564127ce7afd2250815fa
hermia <hermia.wu@outlook.com>
2025-06-27 19:21:21 +0800

    msdata

---
+++ msdata.py
@@ -0,0 +1,14 @@
+import random
+gezi = []
+for i in range(9):
+    gezi.append([])
+    for j in range(9):
+       gezi[i].append(False)
+a=0
+while a<10:
+    c=random.randint(0,80)
+    e=c//9
+    f=c%9
+    if not gezi[e][f]:
+        gezi[e][f]=True
+        a=a+1
